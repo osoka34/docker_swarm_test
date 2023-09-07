f99f387ba3150c3691c4ea6451b5a6ab93f41ca3
alexander <invral@yandex.ru>
2023-09-07 11:42:23 +0300

    Initial commit

---
+++ cmd2/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/ping", func(c *fiber.Ctx) error {
+		// Непрерывно отправлять "pong" каждую секунду
+		//go func() {
+		//for {
+		//c.Context().Response.Reset()
+		time.Sleep(1 * time.Second)
+		err := c.SendString("pong 2\n")
+		if err != nil {
+			return err
+		}
+		//c.Context().Response.Reset() // Очистить буфер перед отправкой следующего "pong"
+		//time.Sleep(1 * time.Second)
+		//}
+		//}()
+		return nil
+	})
+
+	port := 40000
+	fmt.Printf("Сервер запущен на порту %d\n", port)
+
+	err := app.Listen(fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+}
